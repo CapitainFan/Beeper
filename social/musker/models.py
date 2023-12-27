086fe9fa2415a899e57e7620a93da656c26cbc47
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2023-12-28 01:25:38 +0300

    add meep's model and meeps list at home page and profile page

--- social/musker/models.py
+++ social/musker/models.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+class Meep(models.Model):
+	user = models.ForeignKey(
+		User, related_name="meeps", 
+		on_delete=models.DO_NOTHING
+		)
+	body = models.CharField(max_length=200)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return(
+			f"{self.user} "
+			f"({self.created_at:%Y-%m-%d %H:%M}): "
+			f"{self.body}..."
+			)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField("self",
@@ -13,6 +30,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
 
 # Create Profile When New User Signs Up
 @receiver(post_save, sender=User)
--- social/musker/views.py
+++ social/musker/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Meep
 
 
 def home(request):
-	return render(request, 'home.html', {})
+	if request.user.is_authenticated:
+		meeps = Meep.objects.all().order_by("-created_at")
+
+	return render(request, 'home.html', {"meeps":meeps})
 
 
 def profile_list(request):
 	if request.user.is_authenticated:
 		profiles = Profile.objects.exclude(user=request.user)
-		# profiles = Profile.objects.all()
 		return render(request, 'profile_list.html', {"profiles":profiles})
 	else:
 		messages.success(request, ("You Must Be Logged In To View This Page..."))
 		return redirect('home')
 
-
 def profile(request, pk):
 	if request.user.is_authenticated:
 		profile = Profile.objects.get(user_id=pk)
+		meeps = Meep.objects.filter(user_id=pk).order_by("-created_at")
 
 		# Post Form logic
 		if request.method == "POST":
@@ -35,7 +37,9 @@ def profile(request, pk):
 			# Save the profile
 			current_user_profile.save()
 
-		return render(request, 'profile.html', {"profile":profile})
+
+
+		return render(request, "profile.html", {"profile":profile, "meeps":meeps})
 	else:
 		messages.success(request, ("You Must Be Logged In To View This Page..."))
 		return redirect('home')
